db00bdc3654ce8f07cdc9536cf5ce22aa818a398
Emilia ILIE <eilie@assurone-group.com>
2018-03-12 23:25:53 +0100

    Initial commit

---
+++ WalletKata/Users/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace WalletKata.Users
+{
+    public class User
+    {
+        private List<User> friends = new List<User>();
+
+        public IEnumerable GetFriends()
+        {
+            return friends;
+        }
+
+        public void AddFriend(User friend)
+        {
+            friends.Add(friend);
+        }
+    }
+}
